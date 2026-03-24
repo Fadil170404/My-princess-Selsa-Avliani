4c08eccbe9075f2fcd2869cc2077fe89e1337feb
Fadil170404 <fadilbatam1704@gmail.com>
2026-03-25 02:39:59 +0700

    music.js

--- js/music.js
+++ js/music.js
@@ -4,20 +4,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (openBtn && audio) {
     openBtn.addEventListener('click', function(e) {
-      e.preventDefault(); // Tahan dulu pindah halamannya
+      e.preventDefault(); 
       const targetUrl = this.getAttribute('href');
 
       console.log("Mencoba memutar musik...");
       
       audio.play().then(() => {
         console.log("Musik berhasil diputar!");
-        // Tunggu sebentar agar suara terdengar baru pindah halaman
         setTimeout(() => {
           window.location.href = targetUrl;
         }, 800); 
       }).catch(err => {
         console.error("Gagal putar musik. Error:", err);
-        // Jika gagal tetap pindah halaman supaya tidak macet
         window.location.href = targetUrl;
       });
     });
